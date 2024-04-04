3034de54fccf481769ec36ec31bc7440e76ae64a
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-04 18:45:14 +0300

    add validate userSchema

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,23 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 import {hash} from "bcrypt"
+import { z } from "zod"
+
+const userSchema = z
+    .object({
+        username: z.string().min(1, 'Username is required').max(100),
+        email: z.string().min(1, 'Email is required').email('Invalid email'),
+        password: z
+        .string()
+        .min(1, 'Password is required')
+        .min(8, 'Password must have than 8 characters'),
+        confirmPassword: z.string().min(1, 'Password confirmation is required'),
+  })
 
 export async function POST(req: Request) {
     try{
         const body = await req.json();
-        const {email, username, password} = body;
+        const {email, username, password} = userSchema.parse(body);
 
         //email
         const existingUserByEmail = await db.user.findUnique({
