2607ee782ffc18fb59c76a68b6f078adb8be6f3f
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-04 12:41:01 +0300

    add auth form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button variant="default">Кликни по мне</Button>
+      <nav>
+          <Link href="/auth">auth</Link>
+      </nav>
     </main>
   );
 }
