f9878e41089f43e488afecd8399f58b17cba9115
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-06 17:48:20 +0300

    add auth func

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import {PrismaAdapter} from '@next-auth/prisma-adapter'
 import {db} from "./db"
+import { compare } from "bcrypt";
 
 export const authOptions : NextAuthOptions = {
 
@@ -14,20 +15,36 @@ export const authOptions : NextAuthOptions = {
     },
     providers: [
         CredentialsProvider({
-          name: "Credentials",
-          credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-      
-            if (user) {
-              return user
-            } else {
-              return null    
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "exlample@mail.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email || !credentials.password){
+                    return null;
+                }
+
+                const existingUser = await db.user.findUnique({
+                        where: {email: credentials?.email}
+                });
+                if(!existingUser){
+                        return null;
+                }
+                
+                const passwordMatch = await compare(credentials.password, existingUser.password)
+
+                if(!passwordMatch){
+                    return null;
+                }
+                
+                return{
+                    id: `${existingUser.id}`,
+                    username: existingUser.username,
+                    email: existingUser.email
+                }
             }
-          }
+
         })
       ]
 }
