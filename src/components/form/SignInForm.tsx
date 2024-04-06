eab1949e0a62ddc902a06b2f232b5fe895020c29
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-06 17:21:27 +0300

    add SignInForm

---
+++ src/components/form/SignInForm.tsx
@@ -0,0 +1,112 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Toaster, toast } from 'sonner'
+import { useRouter } from "next/navigation"
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+
+const FormSchema = z
+  .object({
+    email: z.string().min(1, 'Обязательное поле').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Обязательное поле')
+      .min(8, 'Пароль должен содержать не менее 8 символов'),
+  })
+
+export default function SignInForm() {
+    const router = useRouter();
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+          email: '',
+          password: '',
+        },
+      });
+      
+    const onSubmit =  async (values: z.infer<typeof FormSchema>) => {
+        const response = await fetch('/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: values.email,
+                password: values.password
+            })
+        })
+        if(response.ok) {
+            toast.success("Авторизация прошла успешно прошла успешно");
+            setTimeout(() => {
+                router.push("/");
+              }, 1000); 
+        }else{
+            toast.error('Ошибка при авторизации')
+        }
+    };
+
+    return (
+        <main className="flex justify-center items-center h-screen p-5">
+            <Toaster richColors  />
+            <Card className="w-full max-w-sm ">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Вход в систему</CardTitle>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
+                            <div className='space-y-2'>
+                            <FormField
+                                control={form.control}
+                                name='email'
+                                render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                    <Input placeholder='mail@example.com' {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name='password'
+                                render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Пароль</FormLabel>
+                                    <FormControl>
+                                    <Input
+                                        type='password'
+                                        placeholder='Введите пароль'
+                                        {...field}
+                                    />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                                )}
+                            />
+                            </div>
+                            <Button className='w-full mt-6' type='submit'>
+                                Войти в систему
+                            </Button>
+                        </form>
+                    </Form>
+                </CardContent>
+            </Card>
+        </main>
+      
+    )
+}
