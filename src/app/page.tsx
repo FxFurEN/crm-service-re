586f5c4ceefb3164d85bae64709529ae67f4a6d6
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-08 21:12:56 +0300

    add navbar in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="default">Кликни по мне</Button>
-      <nav>
-          <Link href="/auth">auth</Link>
-      </nav>
+      <Navbar/>
     </main>
   );
 }
