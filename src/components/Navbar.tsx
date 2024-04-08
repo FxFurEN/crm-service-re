4baa17b3bdfea4ebe751f96b3372542c52f43493
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-08 21:49:24 +0300

    redesign navbar again

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 "use client"
+import React, { useState } from "react";
 import Link from "next/link";
 import {
     Tooltip,
@@ -11,116 +12,85 @@ import {
     Home,
     LineChart,
     Package,
-    Package2,
     Settings,
     ShoppingBag,
     ShoppingCart,
     Users2,
-} from "lucide-react"
+} from "lucide-react";
+
+interface ButtonData {
+    icon: React.ElementType;
+    label: string;
+}
+
+const buttonsData: ButtonData[] = [
+    { icon: Home, label: "Главная" },
+    { icon: ShoppingBag, label: "Заказы" },
+    { icon: Users2, label: "Клиенты" },
+    { icon: Package, label: "Склад" },
+    { icon: ShoppingCart, label: "Магазин" },
+    { icon: LineChart, label: "Отчет" },
+];
 
 export default function Navbar() {
-  return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-            <Link
-                href="#"
-                className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-            >
-                <BrainCog className="h-4 w-4 transition-all group-hover:scale-110" />
-                <span className="sr-only">Acme Inc</span>
-            </Link>
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                    <Link
-                        href="#"
-                        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    >
-                        <Home className="h-5 w-5" />
-                        <span className="sr-only">Главная</span>
-                    </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Главная</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                    <Link
-                        href="#"
-                        className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    >
-                        <ShoppingBag className="h-5 w-5" />
-                        <span className="sr-only">Заказы</span>
-                    </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Заказы</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                    <Link
-                        href="#"
-                        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    >
-                        <Users2 className="h-5 w-5" />
-                        <span className="sr-only">Клиенты</span>
-                    </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Клиенты</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                    <Link
-                        href="#"
-                        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    >
-                        <Package className="h-5 w-5" />
-                        <span className="sr-only">Склад</span>
-                    </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Склад</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                    <Link
-                        href="#"
-                        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    >
-                        <ShoppingCart className="h-5 w-5" />
-                        <span className="sr-only">Магазин</span>
-                    </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Магазин</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                    <Link
-                        href="#"
-                        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    >
-                        <LineChart className="h-5 w-5" />
-                        <span className="sr-only">Отчет</span>
-                    </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Отчет</TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
-            
-        </nav>
-        <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                    <Link
-                        href="#"
-                        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    >
-                        <Settings className="h-5 w-5" />
-                        <span className="sr-only">Настройки</span>
-                    </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Настройки</TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
-        </nav>
-    </aside>
-  );
+    const [activeButton, setActiveButton] = useState<number | null>(null);
+
+    const handleButtonClick = (buttonIndex: number) => {
+        setActiveButton((prevState) => (prevState === buttonIndex ? null : buttonIndex));
+    };
+
+    return (
+        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+            <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+                <Link
+                    href="#"
+                    className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+                >
+                    <BrainCog className="h-4 w-4 transition-all group-hover:scale-110" />
+                    <span className="sr-only">Acme Inc</span>
+                </Link>
+                <TooltipProvider>
+                    {buttonsData.map((button, index) => (
+                        <Tooltip key={index}>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className={`flex h-9 w-9 items-center justify-center rounded-lg ${
+                                        activeButton === index
+                                            ? "bg-accent text-accent-foreground"
+                                            : "text-muted-foreground"
+                                    } transition-colors hover:text-foreground md:h-8 md:w-8`}
+                                    onClick={() => handleButtonClick(index)}
+                                >
+                                    <button.icon className="h-5 w-5" />
+                                    <span className="sr-only">{button.label}</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">{button.label}</TooltipContent>
+                        </Tooltip>
+                    ))}
+                </TooltipProvider>
+            </nav>
+            <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link
+                                href="#"
+                                className={`flex h-9 w-9 items-center justify-center rounded-lg ${
+                                    activeButton === null ? "bg-accent text-accent-foreground" : "text-muted-foreground"
+                                } transition-colors hover:text-foreground md:h-8 md:w-8`}
+                                onClick={() => setActiveButton(null)}
+                            >
+                                <Settings className="h-5 w-5" />
+                                <span className="sr-only">Настройки</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Настройки</TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            </nav>
+        </aside>
+    );
 }
+
