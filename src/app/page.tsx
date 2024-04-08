facb04d0dc8e71009318f99909e9a0a30a87b394
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-08 23:30:32 +0300

    cut navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Navbar from "@/components/Navbar";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Navbar/>
+      
     </main>
   );
 }
