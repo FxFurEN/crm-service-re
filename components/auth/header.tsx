763c62cc5af65440f44848cd0e31edb2b0b589b3
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-14 17:04:29 +0300

    rename header

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -20,7 +20,7 @@ export const Header = ({
         "text-3xl font-semibold",
         font.className,
       )}>
-        🔐 Auth
+        CRM-SERVICE
       </h1>
       <p className="text-muted-foreground text-sm">
         {label}
