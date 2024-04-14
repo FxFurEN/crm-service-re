ca3350001c9708af5db869791c128baeeac42a13
fxfuren <nikitostimoshenko@gmail.com>
2024-04-14 22:56:05 +0300

    cut center

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -7,7 +7,7 @@ interface ProtectedLayoutProps {
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   return ( 
-    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center">
+    <div className="h-full w-full flex flex-col gap-y-10 items-center">
       <Navbar/>
       <Navheader />
       {children}
