2022eb9ebe799db5376c5f2dbd36b04275142f93
fxfuren <nikitostimoshenko@gmail.com>
2024-04-14 22:53:09 +0300

    cut center

--- app/(protected)/_components/navheader.tsx
+++ app/(protected)/_components/navheader.tsx
@@ -10,7 +10,7 @@ export const Navheader = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
+    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[100%] shadow-sm">
       <div className="flex gap-x-2">
         <Button 
           asChild
