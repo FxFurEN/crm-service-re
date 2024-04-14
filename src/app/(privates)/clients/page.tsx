00b9ce711d8e3f32a2bd8f60c8d331896691110d
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-14 15:20:00 +0300

    del old src

--- src/app/(privates)/clients/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ClientsPage() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>ClientsPage</p>
-      </main>
-    );
-  }
-  
--- src/app/(privates)/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SettingsPage() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>SettingsPage</p>
-      </main>
-    );
-  }
-  
