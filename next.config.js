b854ea273456960600d42301a01751298ed5a36f
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-14 17:43:34 +0300

    fix default page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/auth/login', 
+          permanent: true,
+          basePath: false
+        },
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
+  
