6c33a16fcd78d819a45d71e931ab0040855b08cd
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-14 17:45:07 +0300

    cut depend

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import { Poppins } from "next/font/google";
-
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { LoginButton } from "@/components/auth/login-button";
-
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"]
-})
-
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
