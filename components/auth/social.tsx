8069beec2b3c3bbe3960e254d18930377eb9e400
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-14 16:31:07 +0300

    cut github auth

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -12,7 +12,7 @@ export const Social = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
 
-  const onClick = (provider: "google" | "github") => {
+  const onClick = (provider: "google") => {
     signIn(provider, {
       callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
     });
@@ -27,14 +27,6 @@ export const Social = () => {
         onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
-      </Button>
-      <Button
-        size="lg"
-        className="w-full"
-        variant="outline"
-        onClick={() => onClick("github")}
-      >
-        <FaGithub className="h-5 w-5" />
       </Button>
     </div>
   );
