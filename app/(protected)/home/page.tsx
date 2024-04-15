9b779a99933f16688f0fe9994b1f2775a7954c25
fxfuren <nikitostimoshenko@gmail.com>
2024-04-15 23:40:15 +0300

    add barchart in home page

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -1,11 +1,71 @@
 "use client";
+import BarChart from "@/components/BarChart";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-import { UserInfo } from "@/components/user-info";
-import { useCurrentUser } from "@/hooks/use-current-user";
+const data = [
+  {
+    name: "Jan",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Feb",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Mar",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Apr",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "May",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Jun",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Jul",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Aug",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Sep",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Oct",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Nov",
+    total: Math.floor(Math.random() * 5000) + 1000
+  },
+  {
+    name: "Dec",
+    total: Math.floor(Math.random() * 5000) + 1000
+  }
+];
 
 const HomePage = () => {
-  return ( 
-    <p>Home</p>
+  return (
+    <>
+      <Card className="w-[450px]">
+        <CardHeader>
+          <CardTitle>Test chart</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <BarChart data={data} xKey="month" yKey="total" />
+        </CardContent>
+      </Card>
+    </>
+    
    );
 }
  
