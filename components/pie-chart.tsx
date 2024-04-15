0dd32bd1158c8eec815e0b1dfb9f2182cdb5aeb4
fxfuren <nikitostimoshenko@gmail.com>
2024-04-16 00:02:47 +0300

    add PieChart component

---
+++ components/pie-chart.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import {
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  Cell
+} from "recharts";
+
+type DataItem = {
+  [key: string]: string | number;
+};
+
+type Props = {
+  data: DataItem[];
+  dataKey: string;
+};
+
+export default function PieChartComponent({ data, dataKey }: Props) {
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+  return (
+    <ResponsiveContainer width={"100%"} height={350}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey={dataKey}
+          outerRadius={80}
+          fill="#8884d8"
+        >
+          {
+            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+          }
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
