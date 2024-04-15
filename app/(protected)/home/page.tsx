908866ee6781156a3b7790722b96223a06e9aef8
fxfuren <nikitostimoshenko@gmail.com>
2024-04-15 16:16:58 +0300

    add home page

---
+++ app/(protected)/home/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { UserInfo } from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const HomePage = () => {
+  return ( 
+    <p>Home</p>
+   );
+}
+ 
+export default HomePage;
