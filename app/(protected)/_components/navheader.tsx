dfc1b72aef33b4342feaa791904978bd055c239a
fxfuren <nikitostimoshenko@gmail.com>
2024-04-19 13:29:10 +0300

    redesign navheader

--- app/(protected)/_components/navheader.tsx
+++ app/(protected)/_components/navheader.tsx
@@ -1,49 +1,12 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/auth/user-button";
 
 export const Navheader = () => {
-  const pathname = usePathname();
 
   return (
-    <nav className="fixed bg-secondary flex justify-between items-center p-4  rounded-xl w-[100%] shadow-sm">
+    <nav className="fixed bg-secondary flex justify-between items-center p-4  rounded-xl w-[100%] shadow-sm h-[3.5em]">
       <div className="flex gap-x-2 ml-20">
-        <Button 
-          asChild
-          variant={pathname === "/server" ? "default" : "outline"}
-        >
-          <Link href="/server">
-            Server
-          </Link>
-        </Button>
-        <Button 
-          asChild
-          variant={pathname === "/client" ? "default" : "outline"}
-        >
-          <Link href="/client">
-            Client
-          </Link>
-        </Button>
-        <Button 
-          asChild
-          variant={pathname === "/admin" ? "default" : "outline"}
-        >
-          <Link href="/admin">
-            Admin
-          </Link>
-        </Button>
-        <Button 
-          asChild
-          variant={pathname === "/settings" ? "default" : "outline"}
-        >
-          <Link href="/settings">
-            Settings
-          </Link>
-        </Button>
       </div>
       <UserButton />
     </nav>
