e9e2aba45572626c27f79741ddb90b204bae503b
fxfuren <nikitostimoshenko@gmail.com>
2024-04-22 23:46:09 +0300

    add pages

---
+++ app/(protected)/settings/employees/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+const SettingsPage = () => {
+  return ( 
+    <>
+        <p className="text-2xl font-semibold text-center">
+          Сотрудники
+        </p>
+    </>
+   );
+}
+ 
+export default SettingsPage;
