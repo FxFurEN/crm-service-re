8ff43c4a38dc707939ecaeb086d8213594629632
fxfuren <nikitostimoshenko@gmail.com>
2024-04-22 14:12:39 +0300

    fix fetch data

--- app/(protected)/clients/[id]/page.tsx
+++ app/(protected)/clients/[id]/page.tsx
@@ -1,44 +1,57 @@
-"use client"
-
-import { useSearchParams } from 'next/navigation'; 
+"use client";
 import { Client } from '@/types/client'; 
+import { useEffect, useState } from 'react';
 
 export default function ClientDetailPage() {
-  const searchParams = useSearchParams(); 
-  const id = searchParams.get('id'); 
+  const id = getIdFromUrl(); // Получаем id из URL
 
-  const clientData: Client[] = [
-    {
-      id: "1",
-      fullName: "John Doe",
-      clientType: "individual",
-      email: "john.doe@example.com",
-      phone: "+1234567890",
-    },
-    {
-      id: "2",
-      fullName: "ABC Inc.",
-      clientType: "corporate",
-      email: "info@abcinc.com",
-      phone: "+9876543210",
-    },
-    {
-      id: "3",
-      fullName: "Jane Smith",
-      clientType: "individual",
-      email: "jane.smith@example.com",
-      phone: "+1987654321",
-    },
-    {
-      id: "4",
-      fullName: "Jane Smith",
-      clientType: "individual",
-      email: "jane.smith@example.com",
-      phone: "+1987654321",
-    },
-  ];
+  const [client, setClient] = useState<Client | null>(null);
 
-  const client = clientData.find((client) => client.id === id);
+  function getIdFromUrl() {
+    const urlSegments = window.location.pathname.split('/');
+    return urlSegments[urlSegments.length - 1]; 
+  }
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const clientData: Client[] = [
+        {
+          id: "1",
+          fullName: "John Doe",
+          clientType: "individual",
+          email: "john.doe@example.com",
+          phone: "+1234567890",
+        },
+        {
+          id: "2",
+          fullName: "ABC Inc.",
+          clientType: "corporate",
+          email: "info@abcinc.com",
+          phone: "+9876543210",
+        },
+        {
+          id: "3",
+          fullName: "Jane Smith",
+          clientType: "individual",
+          email: "jane.smith@example.com",
+          phone: "+1987654321",
+        },
+        {
+          id: "4",
+          fullName: "Jane Smith",
+          clientType: "individual",
+          email: "jane.smith@example.com",
+          phone: "+1987654321",
+        },
+      ];
+
+      const foundClient: Client = clientData.find((client) => client.id === id);
+      setClient(foundClient);
+    };
+
+    fetchData();
+  }, [id]);
 
   return (
     <div>
@@ -50,7 +63,7 @@ export default function ClientDetailPage() {
           <p>{client.phone}</p>
         </div>
       ) : (
-        <p>Client not found</p>
+        <p>Loading...</p>
       )}
     </div>
   );
