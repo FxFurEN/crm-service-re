1bb1c8db69519c4fb6fe8d5d571cec46e82235f9
fxfuren <nikitostimoshenko@gmail.com>
2024-04-22 23:27:57 +0300

    redesign setting page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,80 +1,25 @@
 "use client";
 
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useTransition, useState } from "react";
-import { useSession } from "next-auth/react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { SettingsSchema } from "@/schemas";
-import {
-  Card,
-  CardHeader,
-  CardContent,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { settings } from "@/actions/settings";
-import {
-  Form,
-  FormField,
-  FormControl,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useCurrentUser } from "@/hooks/use-current-user";
-import { FormError } from "@/components/form-error";
-import { FormSuccess } from "@/components/form-success";
-import { UserRole } from "@prisma/client";
+import Link from "next/link";
 
 const SettingsPage = () => {
-  const user = useCurrentUser();
-
-  const [error, setError] = useState<string | undefined>();
-  const [success, setSuccess] = useState<string | undefined>();
-  const { update } = useSession();
-  const [isPending, startTransition] = useTransition();
-
-  const form = useForm<z.infer<typeof SettingsSchema>>({
-    resolver: zodResolver(SettingsSchema),
-    defaultValues: {
-      password: undefined,
-      newPassword: undefined,
-      name: user?.name || undefined,
-      email: user?.email || undefined,
-      role: user?.role || undefined,
-    }
-  });
-
-  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
-    startTransition(() => {
-      settings(values)
-        .then((data) => {
-          if (data.error) {
-            setError(data.error);
-          }
-
-          if (data.success) {
-            update();
-            setSuccess(data.success);
-          }
-        })
-        .catch(() => setError("Что-то пошло не так!"));
-    });
-  }
-
   return ( 
     <>
         <p className="text-2xl font-semibold text-center">
           ⚙️ Настройки
         </p>
+        <div className="flex justify-center mt-8">
+          <Link href="/settings/profile">
+            <Button className="mr-4">Профиль</Button>
+          </Link>
+          <Link href="/settings//employees">
+            <Button className="mr-4">Сотрудники</Button>
+          </Link>
+          <Link href="/settings//position">
+            <Button className="mr-4">Должность</Button>
+          </Link>
+        </div>
     </>
    );
 }
