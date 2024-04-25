43b9e1973aafc136659dae19629a5878510e4d9a
fxfuren <nikitostimoshenko@gmail.com>
2024-04-25 11:18:47 +0300

    redesign skeleton

--- components/skeleton-card.tsx
+++ components/skeleton-card.tsx
@@ -3,11 +3,18 @@ import { Skeleton } from "@/components/ui/skeleton"
 export function SkeletonCard() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+      <div className="flex space-x-3">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      </div>
+      <div className="flex flex-wrap space-x-3">
+        <Skeleton className="h-4 w-[119px]" />
+        <Skeleton className="h-4 w-[119px]" />
+        <Skeleton className="h-4 w-[118px]" />
+        <Skeleton className="h-4 w-[118px]" />
       </div>
     </div>
-  )
+  );
 }
+
+
