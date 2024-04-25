625f2e44b54cb0f7491c1e175d7bcd9f24787f40
fxfuren <nikitostimoshenko@gmail.com>
2024-04-25 17:00:24 +0300

    add dialog confirm

---
+++ components/alert-dialog-confirm.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+type DeleteConfirmationDialogProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onConfirm: () => void;
+  onCancel: () => void;
+};
+
+export default function DeleteConfirmationDialog({
+  open,
+  onOpenChange,
+  onConfirm,
+  onCancel,
+}: DeleteConfirmationDialogProps) {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Вы уверены?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Вы действительно хотите удалить запись о клиенте?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={onCancel}>Отмена</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Удалить</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
