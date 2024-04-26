68b6bffe32d52df8d9dba11439bbcdb9c1d67f99
fxfuren <nikitostimoshenko@gmail.com>
2024-04-27 00:35:01 +0300

    edit func

--- actions/edit-data.ts
+++ actions/edit-data.ts
@@ -2,7 +2,7 @@
 
 import * as z from "zod";
 import { db } from "@/lib/db";
-import { ClientSchema } from "@/schemas";
+import { ClientSchema, ServiceSchema } from "@/schemas";
 
 export const updateClient = async (clientId: string, updatedData: z.infer<typeof ClientSchema>) => {
   try {
@@ -32,6 +32,33 @@ export const updateClient = async (clientId: string, updatedData: z.infer<typeof
     return { success: "Данные клиента успешно обновлены!", client: updatedClient };
   } catch (error) {
     console.error("Error updating client:", error);
+    return { error: "Что-то пошло не так" };
+  }
+};
+
+
+export const updateService = async (serviceId: string, updatedData: z.infer<typeof ServiceSchema>) => {
+  try {
+    const validatedFields = ServiceSchema.safeParse(updatedData);
+    if (!validatedFields.success) {
+      return { error: "Недопустимые поля!" };
+    }
+    const { name, price, category } = validatedFields.data;
+    
+    const updatedService = await db.service.update({
+      where: {
+        id: serviceId,
+      },
+      data: {
+        name,
+        price,
+        category,
+      },
+    });
+
+    return { success: "Данные услуги успешно обновлены!", service: updatedService };
+  } catch (error) {
+    console.error("Error updating service:", error);
     return { error: "Что-то пошло не так" };
   }
 };
