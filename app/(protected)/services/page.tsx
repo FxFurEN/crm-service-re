eec4dcfe1767b7c405cbbf4bbf78ec8be11d7e32
fxfuren <nikitostimoshenko@gmail.com>
2024-04-26 21:10:08 +0300

    add load data on page

--- app/(protected)/services/page.tsx
+++ app/(protected)/services/page.tsx
@@ -3,20 +3,46 @@
 import { useRouter } from 'next/navigation';
 import { UserRole } from "@prisma/client";
 import { useCurrentRole } from "@/hooks/use-current-role";
+import CustomTable, { TableColumn } from '@/components/data-table';
+import { Service } from '@/types/services';
+import React from 'react';
+import { getAllServices } from '@/data/data-load';
+
+const serviceColumns: TableColumn<Service>[] = [
+    { accessorKey: "name", header: "Наименование", cell: ({ row }) => <div>{row.getValue("name")}</div> },
+    { accessorKey: "price", header: "Цена", cell: ({ row }) => <div>{row.getValue("price")}</div> },
+    { accessorKey: "category", header: "Категория", cell: ({ row }) => <div>{row.getValue("category")}</div> },
+  ];
+
 
 const ServicesPage = () => {
   const router = useRouter();
   const role = useCurrentRole();
+  const [services, setServices] = React.useState<Service[]>([]); 
 
   if (role === UserRole.USER) {
     router.back(); 
   }
 
+
+  React.useEffect(() => {
+    fetchData();
+  }, []); 
+
+  const fetchData = async () => {
+    const data = await getAllServices();
+    if (data) {
+    setServices(data);
+    }
+  };
+
   return ( 
     <>
-        <p className="text-2xl font-semibold text-center">
-          Услуги
-        </p>
+        <CustomTable<Service>
+        data={services}
+        columns={serviceColumns}
+        searchableColumns={["name"]}
+      />
     </>
    );
 }
