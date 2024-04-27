e64a24da1930e42b4c756d4e2e9a555dd97d5f85
FxFurEN <nikitostimoshenko@gmail.com>
2024-04-27 05:40:44 +0300

    fix

--- app/(protected)/services/_components/category-list.tsx
+++ app/(protected)/services/_components/category-list.tsx
@@ -14,8 +14,8 @@ const CategoryList: React.FC<CategoryListProps> = ({ categories, onEditCategory,
     <div className="w-full md:w-48 md:flex-shrink-0 mt-7">
       <h4 className="mb-4 text-md font-medium leading-none">
         Категории
-        <Button className="ml-2 rounded-full " variant="ghost" onClick={onCategoryButtonClick}>
-          <Plus size={24}/>
+        <Button className="ml-2 rounded-full " variant="ghost" size="icon" onClick={onCategoryButtonClick}>
+          <Plus size={15}/>
         </Button>
       </h4>
       <ScrollArea className="h-72 w-full md:w-48 rounded-md border">
@@ -24,8 +24,8 @@ const CategoryList: React.FC<CategoryListProps> = ({ categories, onEditCategory,
             {categories.map((category, index) => (
               <div key={index} className="text-sm">
                 {category.name}
-                <Button className="ml-2 rounded-full" variant="ghost" onClick={() => onEditCategory(category.id)}>
-                  <Pencil />
+                <Button className="ml-2 rounded-full" variant="ghost" size="icon" onClick={() => onEditCategory(category.id)}>
+                  <Pencil size={15}/>
                 </Button>
               </div>
             ))}
