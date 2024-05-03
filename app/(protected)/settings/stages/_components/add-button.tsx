44a105b05795862bf9c64c7735af1797862a3a1b
fxfuren <nikitostimoshenko@gmail.com>
2024-05-04 02:32:45 +0300

    addButton for stages

--- app/(protected)/settings/stages/_components/add-button.tsx
+++ app/(protected)/settings/stages/_components/add-button.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { DialogModalStages } from "@/components/stages/dialog-modal";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import React, { useState } from "react";
 
-const addButton = () => {
+const AddButton = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <Button className="mb-4">
+        <>
+        <Button className="mb-4" onClick={() => setOpen(true)}>
             Добавить статус  <Plus className="ml-2 h-4 w-4"/>
         </Button>
+        <DialogModalStages open={open} onOpenChange={setOpen}/>
+        </>
+        
       );
 }
 
-
-
-export default addButton;
+export default AddButton;
