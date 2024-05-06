7d5bb0e988c4da4ecd30ee107d2975321aa925bd
fxfuren <nikitostimoshenko@gmail.com>
2024-05-06 19:27:20 +0300

    fix

--- app/(protected)/clients/[id]/page.tsx
+++ app/(protected)/clients/[id]/page.tsx
@@ -24,7 +24,7 @@ export default function ClientDetailPage() {
   }, [id]);
 
   return (
-    <div>
+    <div className='ml-20'>
       {client ? (
         <Card className="flex flex-col md:flex-row md:w-[700px] w-[400px]">
           <div className="md:w-[50%]">
