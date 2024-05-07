a2e4d44fa29c3b08039f2db7d690f712dff3a2bd
fxfuren <nikitostimoshenko@gmail.com>
2024-05-07 12:32:07 +0300

    add new template

--- documents/tempates.ts
+++ documents/tempates.ts
@@ -259,3 +259,260 @@ export const  certificate_of_Completion: Template = {
     },
     "pdfmeVersion": "4.0.0"
   }
+
+
+
+
+
+  export const  collection_slip: Template = {
+    "schemas": [
+      {
+        "head copy": {
+          "type": "readOnlyText",
+          "position": {
+            "x": 6.31,
+            "y": 9.88
+          },
+          "content": "Приемная квитанция",
+          "width": 110.08,
+          "height": 11.3,
+          "rotate": 0,
+          "alignment": "left",
+          "verticalAlignment": "middle",
+          "fontSize": 24,
+          "lineHeight": 1,
+          "characterSpacing": 0,
+          "fontColor": "#000000",
+          "backgroundColor": "",
+          "opacity": 1,
+          "readOnly": true,
+        },
+        "text-garant": {
+          "type": "readOnlyText",
+          "position": {
+            "x": 5.4,
+            "y": 86.57
+          },
+          "content": "Условия оказания услуг:\n1. Заказчик соглашается на получение услуг, описанных в заявке или договоре, в соответствии с установленными стандартами и требованиями.\n2. Услуги будут оказаны в соответствии с согласованными сроками и графиком работы. Любые изменения в графике должны быть согласованы сторонами заблаговременно.\n\n3. Стоимость услуг указана в заявке или договоре. Оплата производится в соответствии с условиями, указанными в счете или договоре. Заказчик обязуется оплатить услуги в указанные сроки.\n\n4. Исполнитель несет ответственность за качество и своевременность оказания услуг в соответствии с согласованными условиями. Заказчик обязуется предоставить необходимую информацию и доступ к необходимым ресурсам для выполнения услуг.\n\n5 . В случае выявления недостатков или несоответствия услуг указанным требованиям, Заказчик имеет право на гарантийное обслуживание или рекламацию в соответствии с условиями договора.",
+          "width": 196.33,
+          "height": 83.23,
+          "rotate": 0,
+          "alignment": "left",
+          "verticalAlignment": "top",
+          "fontSize": 12,
+          "lineHeight": 1.1,
+          "characterSpacing": 0,
+          "fontColor": "#000000",
+          "backgroundColor": "",
+          "opacity": 1,
+          "readOnly": true,
+        },
+        "menager": {
+          "type": "readOnlyText",
+          "position": {
+            "x": 5.52,
+            "y": 171.38
+          },
+          "content": "Менеджер: ",
+          "width": 82.58,
+          "height": 27.41,
+          "rotate": 0,
+          "alignment": "left",
+          "verticalAlignment": "top",
+          "fontSize": 13,
+          "lineHeight": 1,
+          "characterSpacing": 0,
+          "fontColor": "#000000",
+          "backgroundColor": "",
+          "opacity": 1,
+          "readOnly": true,
+        },
+        "field9": {
+          "type": "line",
+          "icon": "\u003Csvg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-minus\"\u003E\u003Cpath d=\"M5 12h14\"/\u003E\u003C/svg\u003E",
+          "position": {
+            "x": 32.52,
+            "y": 178.69
+          },
+          "width": 50,
+          "height": 0.5,
+          "rotate": 0,
+          "opacity": 1,
+          "readOnly": true,
+          "color": "#000000"
+        },
+        "client": {
+          "type": "readOnlyText",
+          "position": {
+            "x": 121.01,
+            "y": 171.85
+          },
+          "content": "Клиент: ",
+          "width": 82.58,
+          "height": 27.41,
+          "rotate": 0,
+          "alignment": "left",
+          "verticalAlignment": "top",
+          "fontSize": 13,
+          "lineHeight": 1,
+          "characterSpacing": 0,
+          "fontColor": "#000000",
+          "backgroundColor": "",
+          "opacity": 1,
+          "readOnly": true,
+        },
+        "field9 copy 2": {
+          "type": "line",
+          "icon": "\u003Csvg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-minus\"\u003E\u003Cpath d=\"M5 12h14\"/\u003E\u003C/svg\u003E",
+          "position": {
+            "x": 141.58,
+            "y": 178.69
+          },
+          "width": 50,
+          "height": 0.5,
+          "rotate": 0,
+          "opacity": 1,
+          "readOnly": true,
+          "color": "#000000"
+        },
+        "orderData": {
+          "type": "table",
+          "position": {
+            "x": 4.54,
+            "y": 29.22
+          },
+          "width": 196.19,
+          "height": 56.3432,
+          "content": "[[\"Eggshell Camisole Top\",\"12323\",\"Row 1\",\"Row 1\"],[\"{stage.name}\",\"231\",\"Row 2\",\"Row 2\"]]",
+          "showHead": true,
+          "head": [
+            "Клиент",
+            "Услуга",
+            "Дата обращения",
+            "Предварительная дата выполнения"
+          ],
+          "headWidthPercentages": [25.4163383953596, 21.6217885237806, 27.4501023541454, 25.5117707267144],
+          "tableStyles": {
+            "borderWidth": 0,
+            "borderColor": "#000000"
+          },
+          "headStyles": {
+            "fontSize": 15,
+            "characterSpacing": 0,
+            "alignment": "left",
+            "verticalAlignment": "middle",
+            "lineHeight": 1,
+            "fontColor": "#000000",
+            "borderColor": "#000000",
+            "backgroundColor": "",
+            "borderWidth": {
+              "top": 0.1,
+              "right": 0.1,
+              "bottom": 0.1,
+              "left": 0.1
+            },
+            "padding": {
+              "top": 5,
+              "right": 5,
+              "bottom": 5,
+              "left": 5
+            }
+          },
+          "bodyStyles": {
+            "fontSize": 13,
+            "characterSpacing": 0,
+            "alignment": "center",
+            "verticalAlignment": "middle",
+            "lineHeight": 1,
+            "fontColor": "#000000",
+            "borderColor": "#000000",
+            "backgroundColor": "",
+            "alternateBackgroundColor": "",
+            "borderWidth": {
+              "top": 0.1,
+              "right": 0.1,
+              "bottom": 0.1,
+              "left": 0.1
+            },
+            "padding": {
+              "top": 6,
+              "right": 5,
+              "bottom": 5,
+              "left": 5
+            }
+          },
+          "columnStyles": {
+            "alignment": {
+              "0": "left"
+            }
+          }
+        },
+        "head copy 2": {
+          "type": "readOnlyText",
+          "position": {
+            "x": 118.35,
+            "y": 5.34
+          },
+          "content": "ООО \"Метр Заславль\"\nул. Дзержинская 26",
+          "width": 82.3,
+          "height": 22.68,
+          "rotate": 0,
+          "alignment": "right",
+          "verticalAlignment": "middle",
+          "fontSize": 13,
+          "lineHeight": 1,
+          "characterSpacing": 0,
+          "fontColor": "#000000",
+          "backgroundColor": "",
+          "opacity": 1,
+          "readOnly": true,
+        },
+        "head copy 3": {
+          "type": "readOnlyText",
+          "position": {
+            "x": 8.63,
+            "y": 21.22
+          },
+          "content": "Заказ от ",
+          "width": 27,
+          "height": 7.59,
+          "rotate": 0,
+          "alignment": "left",
+          "verticalAlignment": "middle",
+          "fontSize": 13,
+          "lineHeight": 1,
+          "characterSpacing": 0,
+          "fontColor": "#000000",
+          "backgroundColor": "",
+          "opacity": 1,
+          "readOnly": true,
+        },
+        "createdAt": {
+          "type": "text",
+          "position": {
+            "x": 29.22,
+            "y": 22.43
+          },
+          "icon": "\u003Csvg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-text-cursor-input\"\u003E\u003Cpath d=\"M5 4h1a3 3 0 0 1 3 3 3 3 0 0 1 3-3h1\"/\u003E\u003Cpath d=\"M13 20h-1a3 3 0 0 1-3-3 3 3 0 0 1-3 3H5\"/\u003E\u003Cpath d=\"M5 16H4a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2h1\"/\u003E\u003Cpath d=\"M13 8h7a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-7\"/\u003E\u003Cpath d=\"M9 7v10\"/\u003E\u003C/svg\u003E",
+          "content": "05.05.2024",
+          "width": 45,
+          "height": 7.62,
+          "rotate": 0,
+          "alignment": "left",
+          "verticalAlignment": "top",
+          "fontSize": 13,
+          "lineHeight": 1,
+          "characterSpacing": 0,
+          "fontColor": "#000000",
+          "backgroundColor": "",
+          "opacity": 1
+        }
+      }
+    ],
+    "basePdf": {
+      "width": 210,
+      "height": 297,
+      "padding": [0, 0, 0, 0]
+    },
+    "pdfmeVersion": "4.0.0"
+  }
