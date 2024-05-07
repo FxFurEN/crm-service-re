26b6bdec7273609b77c0d41f52bae70b3162be21
fxfuren <nikitostimoshenko@gmail.com>
2024-05-07 12:32:23 +0300

    fix selectedTemplate

--- components/templates/dialog-modal.tsx
+++ components/templates/dialog-modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { generate } from '@pdfme/generator';
-import { certificate_of_Completion } from '@/documents/tempates';
+import { certificate_of_Completion, collection_slip } from '@/documents/tempates';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
@@ -9,23 +9,22 @@ import { Label } from "@/components/ui/label";
 import { useState, useEffect } from 'react';
 import { text, image, readOnlyText, readOnlySvg, tableBeta, line  } from "@pdfme/schemas";
 import { getOrderById } from '@/actions/data-load';
+import { Template } from "@pdfme/common";
+import { formatDate } from "date-fns";
 
 export function DialogModal({ open, onOpenChange, orderId }) {
-  const [selectedTemplate, setSelectedTemplate] = useState<string>("");
+  const [selectedTemplateName, setSelectedTemplateName] = useState<string>("");
   const [order, setOrder] = useState(null);
 
-  const templates = [
-    { name: "certificate_of_Completion", label: "Акт выполненных работ" },
-    { name: "товарный чек", label: "Товарный чек" },
-    { name: "квитанция о приеме на ремонт", label: "Квитанция о приеме на ремонт" },
-    { name: "гарантийная квитанция", label: "Гарантийная квитанция" }
+  const templates: { name: string; label: string; template: Template; }[] = [
+    { name: "certificate_of_Completion", label: "Акт выполненных работ", template: certificate_of_Completion },
+    { name: "collection_slip", label: "Квитанция о приеме на ремонт", template: collection_slip },
   ];
 
 
   useEffect(() => {
     fetchOrder();
   }, [orderId]);
-
 
   const fetchOrder = async () => {
     if (orderId) {
@@ -34,28 +33,43 @@ export function DialogModal({ open, onOpenChange, orderId }) {
     }
   };
 
-  const handleTemplateSelection = (template: string) => {
-    setSelectedTemplate(template);
+  const handleTemplateSelection = (templateName: string) => {
+    setSelectedTemplateName(templateName);
   };
 
   const generatePDF = () => {
-    if (!certificate_of_Completion) {
-      console.error('Template is not loaded yet.');
-      return;
-    }
+    const selectedTemplate = templates.find(template => template.name === selectedTemplateName);
 
-    const orderData = `[[\"${order.service.name}\",\"${order.service.price}\"]]`;
+    let inputs = []; 
 
-    const clientData = order.client.sign === false ?
-      `${order.client.initials}\n${order.client.phone}\n${order.client.email}` :
-      `${order.client.name}\n${order.client.unp}\n${order.client.email}\n${order.client.phone}`;
+  if (selectedTemplateName === 'certificate_of_Completion') {
+      const orderData = `[[\"${order.service.name}\",\"${order.service.price}\"]]`;
 
-    const inputs = [{
-      orderData: orderData,
-      clientData: clientData
-    }];
-    const plugins = { text, image, readOnlyText, readOnlySvg, Table: tableBeta, line  };
-    generate({ template: certificate_of_Completion, inputs, plugins}).then((pdf) => {
+      const clientData = order.client.sign === false ?
+        `${order.client.initials}\n${order.client.phone}\n${order.client.email}` :
+        `${order.client.name}\n${order.client.unp}\n${order.client.email}\n${order.client.phone}`;
+
+      inputs.push({
+        orderData: orderData,
+        clientData: clientData
+      });
+  } else if (selectedTemplateName === 'collection_slip') {
+      const createdAt = formatDate(order.createdAt, "dd.MM.yyyy");
+      const leadTime = formatDate(order.leadTime, "dd.MM.yyyy");
+
+      const orderData = order.client.sign === false ?
+      `[[\"${order.client.initials}\", \"${order.service.name}\", \"${createdAt}\", \"${leadTime}\"]]` :
+      `[[${JSON.stringify(order.client.name)}, \"${order.service.name}\", \"${createdAt}\", \"${leadTime}\"]]`;    
+      
+      inputs.push({
+        orderData: orderData,
+        createdAt: createdAt
+      });
+  }
+    
+
+    const plugins = { text, image, readOnlyText, readOnlySvg, Table: tableBeta, line };
+    generate({ template: selectedTemplate.template, inputs, plugins }).then((pdf) => {
       try {
         const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
         window.open(URL.createObjectURL(blob));
@@ -66,6 +80,7 @@ export function DialogModal({ open, onOpenChange, orderId }) {
     });
   };
 
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[425px]">
@@ -73,7 +88,7 @@ export function DialogModal({ open, onOpenChange, orderId }) {
           <DialogTitle>Изменить статус заказа</DialogTitle>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <RadioGroup value={selectedTemplate} onValueChange={handleTemplateSelection}>
+          <RadioGroup value={selectedTemplateName} onValueChange={handleTemplateSelection}>
             {templates.map(({ name, label }) => (
               <div key={name} className="flex items-center space-x-2">
                 <RadioGroupItem value={name} id={name} />
