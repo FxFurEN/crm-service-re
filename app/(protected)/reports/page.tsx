4167d4c42b05e268b2c8dcfd2f34e4e06612b078
fxfuren <nikitostimoshenko@gmail.com>
2024-05-08 16:53:15 +0300

    add report page

---
+++ app/(protected)/reports/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+
+const reports = [
+  { name: "status-report", label: "Отчет по статусам" },
+  { name: "employee-report", label: "Отчет по сотрудникам" },
+  { name: "date-report", label: "Отчет по датам" }
+];
+
+const ReportPage = () => {
+  return ( 
+    <>
+      <p className="text-2xl font-semibold text-center">
+        Отчеты
+      </p>
+      <ScrollArea >
+        <div className="p-4">
+          {reports.map((report, index) => (
+            <div key={index}>
+              <Link href={`/reports/${report.name}`}>
+                <Button variant="ghost" className="text-sm w-full text-left">
+                  {report.label}
+                </Button>
+              </Link>
+              {index < reports.length - 1 && <Separator className="my-2" />}
+            </div>
+          ))}
+        </div>
+      </ScrollArea>
+    </>
+  );
+}
+
+export default ReportPage;
