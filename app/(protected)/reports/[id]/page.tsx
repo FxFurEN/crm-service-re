96d621e4ef4ede486f632cc60a70d49395da1adf
fxfuren <nikitostimoshenko@gmail.com>
2024-05-08 17:14:32 +0300

    integration and switch columns in table

--- app/(protected)/reports/[id]/page.tsx
+++ app/(protected)/reports/[id]/page.tsx
@@ -1,50 +1,51 @@
 "use client";
 
+import { getOrdersByPeriod, getOrdersByEmployeeAndPeriod } from '@/actions/data-load';
+import { format } from 'date-fns';
 import { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { usePathname } from "next/navigation";
-import { useRouter } from 'next/navigation';
-import { getOrdersByPeriod } from '@/actions/data-load';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Skeleton } from '@/components/ui/skeleton';
-import { format } from 'date-fns'; // Импорт функции форматирования даты
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 const ReportDetailPage = () => {
-    const pathname = usePathname(); 
+    const pathname = usePathname();
     const name = pathname.split('/').pop();
     const router = useRouter();
-    const [selectedPeriod, setSelectedPeriod] = useState(null); 
-    const [orders, setOrders] = useState(null); 
+    const [selectedPeriod, setSelectedPeriod] = useState(null);
+    const [orders, setOrders] = useState(null);
 
     let reportName = "";
+    let fetchData = null;
+
     switch (name) {
-      case "status-report":
-        reportName = "Отчет по статусам";
-        break;
-      case "employee-report":
-        reportName = "Отчет по сотрудникам";
-        break;
-      case "date-report":
-        reportName = "Отчет по датам";
-        break;
-      default:
-        router.back();
+        case "status-report":
+            reportName = "Отчет по статусам";
+            fetchData = getOrdersByPeriod;
+            break;
+        case "employee-report":
+            reportName = "Отчет по сотрудникам";
+            fetchData = getOrdersByEmployeeAndPeriod;
+            break;
+        case "date-report":
+            reportName = "Отчет по датам";
+            fetchData = getOrdersByPeriod;
+            break;
+        default:
+            router.back();
     }
 
     useEffect(() => {
         if (selectedPeriod) {
-            handlePeriodChange(selectedPeriod);
+            fetchData(selectedPeriod).then(data => {
+                setOrders(data);
+            });
         }
-    }, [selectedPeriod]);
-    
-    const handlePeriodChange = async (selectedPeriod) => {
-        const orders = await getOrdersByPeriod(selectedPeriod); 
-        setOrders(orders); 
-    };
+    }, [selectedPeriod, fetchData]);
 
-
-    return ( 
+    return (
         <>
             <p className="text-2xl font-semibold text-center">
                 {reportName}
@@ -70,7 +71,7 @@ const ReportDetailPage = () => {
                     <TableRow>
                         <TableHead>Услуга</TableHead>
                         <TableHead>Дата заказа</TableHead>
-                        <TableHead>Назначенный сотрудник</TableHead>
+                        {name === 'employee-report' && <TableHead>Назначенный сотрудник</TableHead>}
                     </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -78,26 +79,26 @@ const ReportDetailPage = () => {
                         orders.map(order => (
                             <TableRow key={order.id}>
                                 <TableCell>{order.service.name}</TableCell>
-                                <TableCell>{format((order.createdAt), 'dd.MM.yyyy')}</TableCell>
-                                <TableCell>{order.user.name}</TableCell>
+                                <TableCell>{format(order.createdAt, 'dd.MM.yyyy')}</TableCell>
+                                {name === 'employee-report' && <TableCell>{order.user.name}</TableCell>}
                             </TableRow>
                         ))
                     ) : (
                         <>
                             <TableRow>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
+                                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                                {name === 'employee-report' && <TableCell><Skeleton className="h-4 w-full" /></TableCell>}
                             </TableRow>
                             <TableRow>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
+                                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                                {name === 'employee-report' && <TableCell><Skeleton className="h-4 w-full" /></TableCell>}
                             </TableRow>
                             <TableRow>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
-                                <TableCell><Skeleton className="h-4 w-full"/></TableCell>
+                                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                                {name === 'employee-report' && <TableCell><Skeleton className="h-4 w-full" /></TableCell>}
                             </TableRow>
                         </>
                     )}
@@ -108,5 +109,3 @@ const ReportDetailPage = () => {
 };
 
 export default ReportDetailPage;
-
-
