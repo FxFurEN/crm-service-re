5f0a6c42617ce7147950fee31ba401422a3abe56
fxfuren <nikitostimoshenko@gmail.com>
2024-06-22 21:25:09 +0300

    add normal response

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -6,29 +6,37 @@ import { UserRole } from "@prisma/client";
 import Link from "next/link";
 
 const SettingsPage = () => {
-  return ( 
+  return (
     <>
-        <p className="text-2xl font-semibold text-center">
-          ⚙️ Настройки
-        </p>
-        <div className="flex flex-col sm:flex-row justify-center mt-8">
-          <Link href="/settings/profile">
-            <Button className="mr-4 mb-4 sm:mb-0">Профиль</Button>
+      <p className="text-2xl font-semibold text-center">
+        ⚙️ Настройки
+      </p>
+      <div className="flex flex-col sm:flex-row justify-center mt-8">
+        <Link href="/settings/profile">
+          <Button className="w-full sm:w-auto mr-0 mb-4 sm:mr-4 sm:mb-0">
+            Профиль
+          </Button>
+        </Link>
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <Link href="/settings/employees">
+            <Button className="w-full sm:w-auto mr-0 mb-4 sm:mr-4 sm:mb-0">
+              Сотрудники
+            </Button>
           </Link>
-          <RoleGate allowedRole={UserRole.ADMIN}>
-              <Link href="/settings/employees">
-                  <Button className="mr-4 mb-4 sm:mb-0">Сотрудники</Button>
-              </Link>
-              {/*<Link href="/settings/documents">
-                  <Button className="mr-4 mb-4 sm:mb-0">Документы</Button>
-  </Link>*/}
-              <Link href="/settings/stages">
-                  <Button className="mr-4 mb-4 sm:mb-0">Статусы</Button>
-              </Link>
-          </RoleGate>
-        </div>
+          {/*<Link href="/settings/documents">
+              <Button className="w-full sm:w-auto mr-0 mb-4 sm:mr-4 sm:mb-0">
+                Документы
+              </Button>
+            </Link>*/}
+          <Link href="/settings/stages">
+            <Button className="w-full sm:w-auto mr-0 mb-4 sm:mr-4 sm:mb-0">
+              Статусы
+            </Button>
+          </Link>
+        </RoleGate>
+      </div>
     </>
-   );
+  );
 }
- 
+
 export default SettingsPage;
