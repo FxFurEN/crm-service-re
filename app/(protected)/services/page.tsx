21bb9fb60b0750669dee6f812cbb06424b4c355b
FxFurEN <nikitostimoshenko@gmail.com>
2024-07-06 17:45:08 +0000

    add cache

--- app/(protected)/services/page.tsx
+++ app/(protected)/services/page.tsx
@@ -3,8 +3,9 @@ import ServicesPage from "./page.client";
 import { revalidatePath } from 'next/cache'
 import { currentRole } from "@/lib/auth";
 import AccessPage from "@/components/access.denied";
+import { cache } from "react";
 
-const ServerServicesPage = async () => {
+const ServerServicesPage = cache(async () => {
   const userRole = await currentRole();
 
   if (userRole !== 'ADMIN') {
@@ -27,6 +28,6 @@ const ServerServicesPage = async () => {
       initialCategories={categoriesData}
     />
   );
-};
+});
 
 export default ServerServicesPage;
